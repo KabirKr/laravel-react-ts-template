75978c7742f5a63b830477a84663b16c32d1fa5a
Kabir <38161769+KabirKr@users.noreply.github.com>
2023-01-19 13:38:30 +0530

    Update to Inertia js v1

--- resources/client/index.tsx
+++ resources/client/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { createInertiaApp } from "@inertiajs/inertia-react";
-import { InertiaProgress } from "@inertiajs/progress";
+import { createInertiaApp } from "@inertiajs/react";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 
 import "./index.css";
@@ -11,16 +10,18 @@ const appName =
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
+    resolve: (name) => {
+        return resolvePageComponent(
             `./Pages/${name}.tsx`,
             import.meta.glob("./Pages/**/*.tsx")
-        ),
+        );
+    },
     setup({ el, App, props }) {
         const root = createRoot(el);
 
         root.render(<App {...props} />);
     },
+    progress: {
+        color: "#4B5563",
+    },
 });
-
-InertiaProgress.init({ color: "#4B5563" });
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    server: {
+        host: "127.0.0.1",
+        port: 3000,
+    },
     plugins: [
         laravel({
             input: "resources/client/index.tsx",
